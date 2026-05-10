d49d8b77fb978f16237a056137f175ab154c9a23
suryanshbst <suryansh11102005@gmail.com>
2026-05-10 17:34:52 +0530

    final changes done, ready to deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("{ dB url }/food-del")
-    .then(() => console.log("DB Connected"));
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log("DB Connection Error: ", err));
 };
